7d1b355df5c404151b49917bbf157205383c80b7
arthurscarpin <scarpinarthur.dev@gmail.com>
2024-10-29 21:33:43 -0300

    docs: adicionando a documentação swagger da api

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -6,14 +6,32 @@ import (
 	"github.com/arthurscarpin/gin-api-rest/database"
 	"github.com/arthurscarpin/gin-api-rest/models"
 	"github.com/gin-gonic/gin"
+	_ "github.com/swaggo/swag/example/celler/httputil"
 )
 
+// ExibeTodosAlunos godoc
+// @Summary Exibe todos os alunos
+// @Description Rota para listar todos os alunos cadastrados
+// @Tags alunos
+// @Accept json
+// @Produce json
+// @Success 200 {array} models.Aluno
+// @Router /alunos [get]
 func ExibeTodosAlunos(c *gin.Context) {
 	var alunos []models.Aluno
 	database.DB.Find(&alunos)
 	c.JSON(200, alunos)
 }
 
+// Saudacao godoc
+// @Summary Exibe uma saudação personalizada
+// @Description Rota para exibir uma saudação com o nome fornecido
+// @Tags saudacao
+// @Accept json
+// @Produce json
+// @Param nome path string true "Nome do Aluno"
+// @Success 200 {object} map[string]string
+// @Router /saudacao/{nome} [get]
 func Saudacao(c *gin.Context) {
 	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
@@ -21,6 +39,16 @@ func Saudacao(c *gin.Context) {
 	})
 }
 
+// CriaAluno godoc
+// @Summary Cria um aluno
+// @Description Rota para cadastrar um novo aluno
+// @Tags alunos
+// @Accept json
+// @Produce json
+// @Param aluno body models.Aluno true "Dados do Aluno"
+// @Success 200 {object} models.Aluno
+// @Failure 400 {object} map[string]string
+// @Router /alunos [post]
 func CriaAluno(c *gin.Context) {
 	var aluno models.Aluno
 	if err := c.ShouldBindJSON(&aluno); err != nil {
@@ -37,6 +65,16 @@ func CriaAluno(c *gin.Context) {
 	c.JSON(http.StatusOK, aluno)
 }
 
+// BuscaAlunoPorId godoc
+// @Summary Busca aluno por ID
+// @Description Rota para buscar um aluno específico pelo seu ID
+// @Tags alunos
+// @Accept json
+// @Produce json
+// @Param id path string true "ID do Aluno"
+// @Success 200 {object} models.Aluno
+// @Failure 404 {object} map[string]string
+// @Router /alunos/{id} [get]
 func BuscaAlunoPorId(c *gin.Context) {
 	var aluno models.Aluno
 	id := c.Params.ByName("id")
@@ -51,6 +89,15 @@ func BuscaAlunoPorId(c *gin.Context) {
 	c.JSON(http.StatusOK, aluno)
 }
 
+// DeletaAluno godoc
+// @Summary Deleta aluno
+// @Description Rota para deletar um aluno pelo seu ID
+// @Tags alunos
+// @Accept json
+// @Produce json
+// @Param id path string true "ID do Aluno"
+// @Success 200 {object} map[string]string
+// @Router /alunos/{id} [delete]
 func DeletaAluno(c *gin.Context) {
 	var aluno models.Aluno
 	id := c.Params.ByName("id")
@@ -58,6 +105,17 @@ func DeletaAluno(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": "Aluno deletado com sucesso"})
 }
 
+// EditaAluno godoc
+// @Summary Edita aluno
+// @Description Rota para editar as informações de um aluno pelo seu ID
+// @Tags alunos
+// @Accept json
+// @Produce json
+// @Param id path string true "ID do Aluno"
+// @Param aluno body models.Aluno true "Dados do Aluno"
+// @Success 200 {object} models.Aluno
+// @Failure 400 {object} map[string]string
+// @Router /alunos/{id} [put]
 func EditaAluno(c *gin.Context) {
 	var aluno models.Aluno
 	id := c.Params.ByName("id")
@@ -76,6 +134,16 @@ func EditaAluno(c *gin.Context) {
 	c.JSON(http.StatusOK, aluno)
 }
 
+// BuscaAlunoPorCPF godoc
+// @Summary Busca aluno por CPF
+// @Description Rota para buscar um aluno específico pelo seu CPF
+// @Tags alunos
+// @Accept json
+// @Produce json
+// @Param cpf path string true "CPF do Aluno"
+// @Success 200 {object} models.Aluno
+// @Failure 404 {object} map[string]string
+// @Router /alunos/cpf/{cpf} [get]
 func BuscaAlunoPorCPF(c *gin.Context) {
 	var aluno models.Aluno
 	cpf := c.Param("cpf")
--- routes/routes.go
+++ routes/routes.go
@@ -2,11 +2,15 @@ package routes
 
 import (
 	"github.com/arthurscarpin/gin-api-rest/controllers"
+	docs "github.com/arthurscarpin/gin-api-rest/docs"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func HandleRequests() {
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/"
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/assets", "./assets")
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
@@ -18,5 +22,6 @@ func HandleRequests() {
 	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.GET("/index", controllers.ExibePaginaIndex)
 	r.NoRoute(controllers.RotaNaoEncontrada)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.Run()
 }
